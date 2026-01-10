ee8dc9d3ec715ae560ef49fdfdeb10f49c84eb6c
KhondamirChess <hondamirkenjaboev@gmail.com>
2026-01-11 04:27:23 +0500

    first commit

---
+++ src/main/java/dev/khondamir/onlinelib/OnlineLibApplication.java
@@ -0,0 +1,13 @@
+package dev.khondamir.onlinelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/khondamir/onlinelib/OnlineLibApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.khondamir.onlinelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
